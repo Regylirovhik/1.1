5d0491a2bc097454a605dd1ccb072155a19e7c39
Егор Пястолов <piastolov.ee@phystech.edu>
2021-02-11 18:58:39 +0300

    мастер пока ничего не знает

--- 1.1/Source.cpp
+++ 1.1/Source.cpp
@@ -6,7 +6,9 @@ int main()
 {
 	std::cout << "Well, lets' start" << std::endl;
 	std::cout << "Alright, the first change is here" << std::endl;
-	std::cout << "change from github << std::endl;
+	std::cout << "change from github" << std::endl;
 
 	
+
+	std::cout << "master is here" << std::endl;
 }
