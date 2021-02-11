4109648fb389ab81e60259c164df83d7b8c01345
Regylirovshik <45238585+Regylirovhik@users.noreply.github.com>
2021-02-11 18:47:46 +0300

    Update Source.cpp
    работаем и на сайте

--- 1.1/Source.cpp
+++ 1.1/Source.cpp
@@ -6,7 +6,7 @@ int main()
 {
 	std::cout << "Well, lets' start" << std::endl;
 	std::cout << "Alright, the first change is here" << std::endl;
-	 
+	std::cout << "change from github << std::endl;
 
 	
 }
